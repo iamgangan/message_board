47a87fb94b7703aad259c712b3ba845ee60f5399
01057553 <01057553@10.17.77.96>
2023-11-17 16:38:28 +0900

    Index/NewServletの修正

--- src/controllers/NewServlet.java
+++ src/controllers/NewServlet.java
@@ -1,9 +1,8 @@
 package controllers;
 
 import java.io.IOException;
-import java.sql.Timestamp;
 
-import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -11,7 +10,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import models.Message;
-import utils.DBUtil;
 
 /**
  * Servlet implementation class NewServlet
@@ -33,6 +31,16 @@ public class NewServlet extends HttpServlet {
      */
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+        //CSRF対策
+        request.setAttribute("_token", request.getSession().getId());
+
+        //インスタンスの生成
+        request.setAttribute("message", new Message());
+
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/messages/new.jsp");
+        rd.forward(request, response);
+
+ /*
         //EntityManagerの取得
         EntityManager em = DBUtil.createEntityManager();
         em.getTransaction().begin();
@@ -58,6 +66,7 @@ public class NewServlet extends HttpServlet {
         //自動採番されたIDの値を表示
         response.getWriter().append(Integer.valueOf(m.getId()).toString());
         response.getWriter().append(m.getCreated_at().toString());
+*/
 
     }
 
